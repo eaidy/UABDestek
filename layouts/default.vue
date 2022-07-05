ec0b98ca7b39b2cb123c4eb98d0fca9ce0b01838
eaidy <caliskanataa@gmail.com>
2022-07-05 18:13:17 +0300

    First

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <div>
+      <v-btn color="secondary">
+        <NuxtLink to="/admin-tickets" style="color:antiquewhite;text-decoration:none;">Admin Tickets</NuxtLink>
+      </v-btn>
+      <v-btn color="secondary">
+        <NuxtLink to="/arama-sonuc" style="color:antiquewhite;text-decoration:none;">Arama Sonuç</NuxtLink>
+      </v-btn>
+      <v-btn color="secondary">
+        <NuxtLink to="/arama" style="color:antiquewhite;text-decoration:none;">Arama</NuxtLink>
+      </v-btn>
+      <v-btn color="secondary">
+        <NuxtLink to="/my-tickets" style="color:antiquewhite;text-decoration:none;">My Tickets</NuxtLink>
+      </v-btn>
+      <v-btn color="secondary">
+        <NuxtLink to="/ticket-ac" style="color:antiquewhite;text-decoration:none;">Ticket Aç</NuxtLink>
+      </v-btn>
+    </div>
+
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
