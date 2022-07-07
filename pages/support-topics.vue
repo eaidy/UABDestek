0b94667b6f7b7d51a76997395e925b1957202dfc
eaidy <caliskanataa@gmail.com>
2022-07-07 19:51:25 +0300

    v-breadcrumbs hizalanması yapıldı

--- pages/support-topics.vue
+++ pages/support-topics.vue
@@ -2,18 +2,21 @@
     <v-container>
         <v-row class="ma-4">
             <v-col cols="12">
-               <div class="header">
-                Uygun Başlıklar
-               </div>
-               <div class="text-center">
+                <div class="header">
+                    Uygun Başlıklar
+                </div>
+                <div>
                     <v-breadcrumbs
                         :items="links"
                         divider="/"
+                        class="justify-center"
                     ></v-breadcrumbs>
-               </div>
+                </div>
+                <v-divider></v-divider>
             </v-col>
         </v-row>
-        <v-row>
+
+        <v-row class="items-section">
             <v-col col="6">
                 <div>
                     <div class="sub-header ml-3">
@@ -21,7 +24,12 @@
                     </div>
                     <div class="ml-6 mt-2">
                         <div v-for="ticket in tickets" :key="ticket.id" class="mb-1">
-                            <v-icon small>mdi-pencil</v-icon><span class="ml-2 list-element"><a href="/support-topics">{{ ticket.subject }}</a></span>
+                            <v-icon small>mdi-pencil</v-icon>
+                            <span class="ml-2 list-element">
+                                <a href="/support-topics">
+                                    {{ ticket.subject }}
+                                </a>
+                            </span>
                         </div>
                     </div>
                 </div>
@@ -32,8 +40,8 @@
                         <v-icon class="mb-1">mdi-file-document-edit</v-icon> Öneri ve Şikayetler
                     </div>
                     <div class="ml-6 mt-2">
-                        <div v-for="ticket in tickets" :key="ticket.id">
-                            <v-icon small>mdi-pencil</v-icon><span class="ml-2 list-element"><a href="/support-topics">{{ ticket.subject }}</a></span>
+                        <div v-for="suggest in suggests" :key="suggest.id">
+                            <v-icon small>mdi-pencil</v-icon><span class="ml-2 list-element"><a href="/support-topics">{{ suggest.subject }}</a></span>
                         </div>
                     </div>
                 </div>
@@ -78,13 +86,19 @@ export default {
             ],
             suggests: [
                 {
-
+                    ticketNo: 1,
+                    subject: 'Proje yönetim sistemi ekranı ile ilgili önerim',
+                    exp: 'Lorem ipsum narem pipsum'
                 },
                 {
-
+                    ticketNo: 2,
+                    subject: 'Destek taleplerinin daha hızlı sonuçlanması gerekiyor',
+                    exp: 'Lorem ipsum narem pipsum'
                 },
                 {
-
+                    ticketNo: 3,
+                    subject: 'Kullanıcı arayüzündeki ergonomik eksiklikler',
+                    exp: 'Lorem ipsum narem pipsum'
                 }
             ]
         }
@@ -118,6 +132,11 @@ export default {
 .list-element a:hover {
     text-decoration: none;
     color:#413F42;
+}
+
+.items-section {
+    background-color: #F1EEE9;
+    border-radius: 10px;
 }
 
 </style>
