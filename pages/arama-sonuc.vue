cd1b171768eed2bbf5fef2c5ca5637e6dad5e1b6
ercangvn <ercangvn@gmail.com>
2022-07-11 21:10:09 +0300

    style update

--- pages/arama-sonuc.vue
+++ pages/arama-sonuc.vue
@@ -1,80 +1,85 @@
 <template>
-  <v-container class="mt-15">
-    <div class="text-center mb-15">
-      <h1 class="title">UAB-UYS DESTEK TALEP SİSTEMİ</h1>
-    </div>
-    <v-text-field
-      label="Aranacak Konu"
-      placeholder="Arama Yapmak İstediğiniz Konu Giriniz"
-      filled
-      rounded
-      dense
-    ></v-text-field>
-    <div class="d-flex justify-space-between mt-1">
-      <v-radio-group v-model="row" row class="mt-1">
-        <v-radio label="Şifre" value="radio-1"></v-radio>
-        <v-radio label="Destek Talebi" value="radio-2"></v-radio>
-        <v-radio label="Öneri / Şikayet" value="radio-3"></v-radio>
-      </v-radio-group>
-      <v-btn color="blue-grey" class="ma-2 white--text" rounded>
-        Talep Oluştur
-        <v-icon right dark> mdi-plus </v-icon>
-      </v-btn>
-    </div>
+  <div>
+    <section class="search-bg">
+      <v-container class="mt-15">
+        <div class="text-center mb-15">
+          <h1 class="page-title">UAB-UYS DESTEK TALEP SİSTEMİ</h1>
+        </div>
+        <v-text-field
+          label="Aranacak Konu"
+          placeholder="Arama Yapmak İstediğiniz Konu Giriniz"
+          filled
+          rounded
+          dense
+        ></v-text-field>
+        <div class="d-flex justify-space-between mt-1">
+          <v-radio-group v-model="row" row class="mt-1">
+            <v-radio label="Şifre" value="radio-1"></v-radio>
+            <v-radio label="Destek Talebi" value="radio-2"></v-radio>
+            <v-radio label="Öneri / Şikayet" value="radio-3"></v-radio>
+          </v-radio-group>
+          <v-btn color="blue-grey" class="ma-2 white--text" rounded>
+            Talep Oluştur
+            <v-icon right dark> mdi-plus </v-icon>
+          </v-btn>
+        </div>
+      </v-container>
+    </section>
+    <v-container>
+      <div class="mt-10">
+        <div class="text-left">
+          <h2 class="title mb-4">Sonuçlar</h2>
+        </div>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Item <small class="result-date ml-4">15.12.2022</small>
+              <template v-slot:actions>
+                <v-icon color="primary"> $expand </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
 
-    <div class="mt-15 card-bg">
-      <div class="text-center">
-        <h2 class="title mb-4">Sonuçlar</h2>
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              Item <small class="result-date ml-4">15.12.2022</small>
+              <template v-slot:actions>
+                <v-icon color="teal"> mdi-check </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              Item <small class="result-date ml-4">15.12.2022</small>
+              <template v-slot:actions>
+                <v-icon color="error"> mdi-alert-circle </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <div class="text-center mt-4">
+          <v-pagination v-model="page" :length="6"></v-pagination>
+        </div>
       </div>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-header>
-            Item <small class="result-date ml-4">15.12.2022</small>
-            <template v-slot:actions>
-              <v-icon color="primary"> $expand </v-icon>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-
-        <v-expansion-panel>
-          <v-expansion-panel-header disable-icon-rotate>
-            Item <small class="result-date ml-4">15.12.2022</small>
-            <template v-slot:actions>
-              <v-icon color="teal"> mdi-check </v-icon>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-
-        <v-expansion-panel>
-          <v-expansion-panel-header disable-icon-rotate>
-            Item <small class="result-date ml-4">15.12.2022</small>
-            <template v-slot:actions>
-              <v-icon color="error"> mdi-alert-circle </v-icon>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-      <div class="text-center mt-4">
-        <v-pagination v-model="page" :length="6"></v-pagination>
-      </div>
-    </div>
-  </v-container>
+    </v-container>
+  </div>
 </template>
--- pages/support-topics.vue
+++ pages/support-topics.vue
@@ -1,212 +1,220 @@
 <template>
-    <v-container>
+  <div>
+    <section class="search-bg">
+      <v-container>
         <v-row class="ma-4">
-            <v-col cols="12">
-                <div class="header">
-                    Uygun Başlıklar
-                </div>
-                <div>
-                    <v-breadcrumbs
-                        :items="links"
-                        divider="/"
-                        class="justify-center"
-                    ></v-breadcrumbs>
-                </div>
-                <v-divider></v-divider>
-                <br />
-                <v-text-field
-                    label="Arama"
-                    placeholder="Arama Yapmak İstediğiniz Konu Giriniz"
-                    outlined
-                ></v-text-field>
-            </v-col>
+          <v-col cols="12">
+            <div class="text-center">
+              <div class="page-title">Uygun Başlıklar</div>
+              <div>
+                <v-breadcrumbs
+                  :items="links"
+                  divider="/"
+                  class="justify-center"
+                ></v-breadcrumbs>
+              </div>
+            </div>
+            <v-divider></v-divider>
+            <br />
+            <v-text-field
+              label="Arama"
+              placeholder="Arama Yapmak İstediğiniz Konu Giriniz"
+              background-color="#fff"
+              outlined
+            ></v-text-field>
+          </v-col>
         </v-row>
-
-        <v-row class="items-section elevation-2">
-            <v-col col="6">
-                <div>
-                    <div class="sub-header ml-3 mb-3">
-                        <v-icon class="mb-1">mdi-file-document-check</v-icon> Destek Talepleri
-                    </div>
-                    <v-divider></v-divider>
-                    <div class="ml-6 mt-4">
-                        <div v-for="ticket in tickets" :key="ticket.id" class="mb-1">
-                            <v-icon small>mdi-pencil</v-icon>
-                            <span class="ml-2 list-element">
-                                <a href="/support-topics">
-                                    {{ ticket.subject }}
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </v-col>
-            <v-col col="6">
-                <div>
-                    <div class="sub-header ml-3 mb-3">
-                        <v-icon class="mb-1">mdi-file-document-edit</v-icon> Öneri ve Şikayetler
-                    </div>
-                    <v-divider></v-divider>
-                    <div class="ml-6 mt-4">
-                        <div v-for="suggest in suggests" :key="suggest.id" class="mb-1">
-                            <v-icon small>mdi-pencil</v-icon><span class="ml-2 list-element"><a href="/support-topics">{{ suggest.subject }}</a></span>
-                        </div>
-                    </div>
-                </div>
-            </v-col>
-        </v-row>
+      </v-container>
+    </section>
+    <v-container class="mt-8">
+      <v-row class="">
+        <v-col col="6">
+          <div class="default-card">
+            <div class="sub-header ml-3 mb-3">
+              <v-icon class="mb-1">mdi-file-document-check</v-icon> Destek
+              Talepleri
+            </div>
+            <v-divider></v-divider>
+            <div class="ml-6 mt-4">
+              <div v-for="ticket in tickets" :key="ticket.id" class="mb-1">
+                <v-icon small>mdi-pencil</v-icon>
+                <span class="ml-2 list-element">
+                  <a href="/support-topics">
+                    {{ ticket.subject }}
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </v-col>
+        <v-col col="6">
+          <div class="default-card">
+            <div class="sub-header ml-3 mb-3">
+              <v-icon class="mb-1">mdi-file-document-edit</v-icon> Öneri ve
+              Şikayetler
+            </div>
+            <v-divider></v-divider>
+            <div class="ml-6 mt-4">
+              <div v-for="suggest in suggests" :key="suggest.id" class="mb-1">
+                <v-icon small>mdi-pencil</v-icon
+                ><span class="ml-2 list-element"
+                  ><a href="/support-topics">{{ suggest.subject }}</a></span
+                >
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
+  </div>
 </template>
 
 <script>
-
 export default {
-    data () {
-        return {
-            links: [
-                {
-                    text: 'Destek-Talep',
-                    disabled: false,
-                    href: 'support-topics',
-                    nuxt: true
-                },
-                {
-                    text: 'Öneri-Şikayet',
-                    disabled: false,
-                    href: 'support-topics',
-                    nuxt: true
-                }
-            ],
-            tickets: [
-                {
-                    ticketNo: 1,
-                    subject: 'Modül 2 içerisindeki yazılım hatası',
-                    exp: 'Lorem ipsum narem pipsum',
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Proje editleme sorunu ile ilgili',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'KML Dosyası eklerken hata alıyorum',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 1,
-                    subject: 'Modül 2 içerisindeki yazılım hatası',
-                    exp: 'Lorem ipsum narem pipsum',
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Proje editleme sorunu ile ilgili',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'KML Dosyası eklerken hata alıyorum',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 1,
-                    subject: 'Modül 2 içerisindeki yazılım hatası',
-                    exp: 'Lorem ipsum narem pipsum',
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Proje editleme sorunu ile ilgili',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'KML Dosyası eklerken hata alıyorum',
-                    exp: 'Lorem ipsum narem pipsum'
-                }
-            ],
-            suggests: [
-                {
-                    ticketNo: 1,
-                    subject: 'Proje yönetim sistemi ekranı ile ilgili önerim',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Destek taleplerinin daha hızlı sonuçlanması gerekiyor',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'Kullanıcı arayüzündeki ergonomik eksiklikler',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 1,
-                    subject: 'Proje yönetim sistemi ekranı ile ilgili önerim',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Destek taleplerinin daha hızlı sonuçlanması gerekiyor',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'Kullanıcı arayüzündeki ergonomik eksiklikler',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 1,
-                    subject: 'Proje yönetim sistemi ekranı ile ilgili önerim',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 2,
-                    subject: 'Destek taleplerinin daha hızlı sonuçlanması gerekiyor',
-                    exp: 'Lorem ipsum narem pipsum'
-                },
-                {
-                    ticketNo: 3,
-                    subject: 'Kullanıcı arayüzündeki ergonomik eksiklikler',
-                    exp: 'Lorem ipsum narem pipsum'
-                }
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      links: [
+        {
+          text: "Destek-Talep",
+          disabled: false,
+          href: "support-topics",
+          nuxt: true,
+        },
+        {
+          text: "Öneri-Şikayet",
+          disabled: false,
+          href: "support-topics",
+          nuxt: true,
+        },
+      ],
+      tickets: [
+        {
+          ticketNo: 1,
+          subject: "Modül 2 içerisindeki yazılım hatası",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Proje editleme sorunu ile ilgili",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "KML Dosyası eklerken hata alıyorum",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 1,
+          subject: "Modül 2 içerisindeki yazılım hatası",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Proje editleme sorunu ile ilgili",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "KML Dosyası eklerken hata alıyorum",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 1,
+          subject: "Modül 2 içerisindeki yazılım hatası",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Proje editleme sorunu ile ilgili",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "KML Dosyası eklerken hata alıyorum",
+          exp: "Lorem ipsum narem pipsum",
+        },
+      ],
+      suggests: [
+        {
+          ticketNo: 1,
+          subject: "Proje yönetim sistemi ekranı ile ilgili önerim",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Destek taleplerinin daha hızlı sonuçlanması gerekiyor",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "Kullanıcı arayüzündeki ergonomik eksiklikler",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 1,
+          subject: "Proje yönetim sistemi ekranı ile ilgili önerim",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Destek taleplerinin daha hızlı sonuçlanması gerekiyor",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "Kullanıcı arayüzündeki ergonomik eksiklikler",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 1,
+          subject: "Proje yönetim sistemi ekranı ile ilgili önerim",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 2,
+          subject: "Destek taleplerinin daha hızlı sonuçlanması gerekiyor",
+          exp: "Lorem ipsum narem pipsum",
+        },
+        {
+          ticketNo: 3,
+          subject: "Kullanıcı arayüzündeki ergonomik eksiklikler",
+          exp: "Lorem ipsum narem pipsum",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 .header {
-    font-size: 2.7em;
-    color: #413F42;
-    text-align: center;
+  font-size: 2.7em;
+  color: #413f42;
+  text-align: center;
 }
 
 .sub-header {
-    font-size: 1.4em;
-    color: #413F42;
+  font-size: 1.4em;
+  color: #413f42;
 }
 
 .list-element {
-    font-size: 0.95em;
-    color: #413F42;
+  font-size: 0.95em;
+  color: #413f42;
 }
 
 .list-element a {
-    text-decoration: none;
-    color:#7F8487;
+  text-decoration: none;
+  color: #7f8487;
 }
 
 .list-element a:hover {
-    text-decoration: none;
-    color:#413F42;
+  text-decoration: none;
+  color: #413f42;
 }
 
 .items-section {
-    background-color: #EFEFEF;
-    border-radius: 10px;
+  background-color: #efefef;
+  border-radius: 10px;
 }
-
 </style>
