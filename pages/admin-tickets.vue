1208e85dc6034053b50b6c28534309cac7e1f686
eaidy <caliskanataa@gmail.com>
2022-07-14 01:35:05 +0300

    ...

--- pages/admin-tickets.vue
+++ pages/admin-tickets.vue
@@ -81,7 +81,182 @@
       :footer-props="{
         'items-per-page-text': 'Sayfa başına satır'
       }"
-    ></v-data-table>
+    >
+      <template #item.actions="{ item }">
+        <v-btn text class="nav-btn" @click="answerDialog = true">
+          Yanıtla
+        </v-btn>
+      </template>
+    </v-data-table>
+      <v-dialog v-model="answerDialog" :width="expandedWidth">
+        <v-card>
+          <v-card-title class="justify-center">
+            Talep Oluştur
+          </v-card-title>
+
+          <v-divider></v-divider>
+          <v-container>
+            <v-row>
+              <v-col class="vertical-rule">
+                <v-card-text class="mt-4">
+                  <v-container>
+                    <v-row>
+                      <v-col cols="6" class="">
+                        <v-select 
+                          label="Modül Seçiniz" 
+                          outlined 
+                          class="mb-6"
+                          :items="modules"
+                        ></v-select>
+                      </v-col>
+                      <v-col cols="6">
+                        <v-select 
+                          label="Ticket Tipini Seçiniz" 
+                          outlined 
+                          class="mb-6"
+                          :items="ticketTypes"
+                        ></v-select>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-text-field label="Konu" outlined class="mb-6"></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-textarea label="Açıklama" outlined></v-textarea>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <div class="d-flex mt-8">
+                          <v-btn color="primary" @click="dialog = false"> Gönder </v-btn>
+                          <v-btn
+                            color="secondary"
+                            text
+                            @click="dialog = false"
+                            class="ml-4"
+                          >
+                            İptal
+                          </v-btn>
+                          <v-spacer></v-spacer>
+                          <div class="admin-ticket-expand-div">
+                            <span v-if="!expandedDialog" @click="expandedDialog = true" class="admin-ticket-expand">Detay >></span>
+                            <span v-if="expandedDialog" @click="expandedDialog = false" class="admin-ticket-expand">&lt&lt Kısalt</span>
+                          </div>
+                        </div>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-divider></v-divider>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <div class="d-flex justify-space-between align-items-center w-100">
+                            <div class="w-300" style="width: 300px">
+                              <v-file-input
+                                accept="image/*"
+                                label="Dosya Ekleyin"
+                                style="width: 300px"
+                              ></v-file-input>
+                            </div>
+                            <v-btn color="primary" text @click="dialog = false">
+                              Çıkış
+                            </v-btn>
+                          </div>
+                        </v-card-actions>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+              </v-col>
+
+              <!-- Expanded kısım -->
+              <v-col v-if="expandedDialog">
+                <v-card-text class="mt-4">
+                  <v-container>
+                    <v-row>
+                      <v-col>
+                        <v-textarea label="Admin Notları" outlined></v-textarea>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-text-field
+                          label="Etiketler"
+                          outlined
+                          v-model="temp.tag"
+                          @keyup.enter="addHandler"
+                        ></v-text-field>
+
+                        <div class="tags">
+                          <span 
+                            class="tag" 
+                            v-for="tag in tags" 
+                            :key="tag"
+                            @click="deleteHandler(tag)"
+                          >{{ tag }}</span>
+                        </div>
+
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-text-field 
+                          label="İlgili Kişiler" 
+                          outlined 
+                          v-model="temp.person"
+                          @keyup.enter="addPerson"
+                        >
+                        </v-text-field>
+                        <div class="tags">
+                          <span 
+                            class="tag" 
+                            v-for="person in people" 
+                            :key="person"
+                            @click="deletePerson(person)"
+                          >{{ person }}</span>
+                        </div>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-subheader>İlgili Makaleleri İliştir</v-subheader>
+                        <v-text-field
+                          label="Arama"
+                          outlined
+                          v-model="ticketSearch"
+                        >
+                        </v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+              </v-col>
+            </v-row>
+
+            <!-- Talep Süreci Diyaloğu -->
+            <v-row>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col>
+                      <h3>Talep Süreci</h3>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+            </v-row>
+          </v-container>
+          
+        </v-card>
+      </v-dialog>
   </v-container>
 </template>
 
@@ -89,6 +264,9 @@
 export default {
   data() {
     return {
+      answerDialog: false,
+      expandedDialog: false,
+      ticketSearch: '',
       headers: [
         {
           text: "Sıra",
@@ -105,6 +283,7 @@ export default {
         { text: "Onaylayan", value: "confirmer" },
         { text: "Durumu", value: "state" },
         { text: "Tarih", value: "enddate" },
+        { text: 'Actions', value: 'actions', sortable: false }
       ],
       tickets: [
         {
@@ -144,13 +323,92 @@ export default {
           enddate: '23.05.2022'
         }
       ],
+      modules: [
+        'Modül 1',
+        'Modül 2',
+        'Modül 3'
+      ],
+      ticketTypes: [
+        'Şifre Değiştirme',
+        'Destek-Talep',
+        'Öneri-Şikayet'
+      ],
+      tags: [],
+      people: [],
+      temp: {
+        tag: '',
+        person: ''
+      }
     };
   },
+
+  computed: {
+    expandedWidth () {
+      return this.expandedDialog ? 1000 : 800
+    }
+  },
+
+  methods: {
+    addHandler (e) {
+        if ( !this.tags.includes(this.temp.tag) ){
+          this.tags.push(this.temp.tag)
+        }
+        this.temp.tag = ''
+    },
+    deleteHandler (tag) {
+      this.tags.splice(this.tags.indexOf(tag), 1)
+    },
+    addPerson () {
+        if ( !this.people.includes(this.temp.person) ){
+          this.people.push(this.temp.person)
+        }
+        this.temp.person = ''
+    },
+    deletePerson (person) {
+      this.people.splice(this.people.indexOf(person), 1)
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .v-text-field.v-text-field--enclosed .v-text-field__details {
   display: none;
+}
+
+.admin-ticket-expand-div {
+  display: flex;
+  align-items:flex-end;
+}
+
+.admin-ticket-expand {
+  font-size: 1.1em;
+}
+
+.admin-ticket-expand:hover {
+  cursor: pointer;
+  color: #7F8487;
+}
+
+.vertical-rule {
+  border-right: 1px solid;
+  border-color: rgba(0, 0, 0, 0.12);
+}
+
+.tag {
+  border-radius: 4px;
+  border: solid 1px gray;
+  margin-right: 4px;
+  padding: 4px;
+}
+
+.tags {
+  padding-left: 4px;
+}
+
+.process-header {
+  border-bottom: 1px solid;
+  border-color: rgba(0, 0, 0, 0.12);
+
 }
 </style>
